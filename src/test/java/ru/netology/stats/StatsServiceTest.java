da54ad7318d954901861e0ae42e6067f67fd4d02
IpHo1 <ilya.cherny2007@gmail.com>
2024-10-02 21:26:31 +0300

    Устранение проблем с переводом строк

--- src/test/java/ru/netology/stats/StatsServiceTest.java
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -1,4 +1,28 @@
 package ru.netology.stats;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class StatsServiceTest {
+
+    @Test
+    public void shouldCalculateSumSales() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 180;
+        long actual = service.sumSales(sales);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindMaxSales() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 8; // Месяц с максимальными продажами (нумерация с 1)
+        int actual = service.maxSales(sales);
+        assertEquals(expected, actual);
+    }
+
+    // Дополнительные тесты на другие методы...
 }
+
